98d8c900bb7123ff6c2c237e783cece64ea3e3f1
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-09-28 20:42:20 +0000

    inicioStarWars

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -9,9 +9,23 @@ export const Navbar = () => {
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+				<div className="dropdown">
+					<button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+						Favorites
+					</button>
+					<ul className="dropdown-menu">
+						<li><a className="dropdown-item" href="#">Action</a></li>
+						<li><a className="dropdown-item" href="#">Another action</a></li>
+						<li><a className="dropdown-item" href="#">Something else here</a></li>
+					</ul>
+					</div>
 				</Link>
 			</div>
+			
+
+
+
+
 		</nav>
 	);
 };
