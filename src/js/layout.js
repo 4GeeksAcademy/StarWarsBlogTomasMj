016a9997bacec551e1bea672c5c02dc53a2c2a0e
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-10-13 02:19:27 +0000

    sw_blog

--- src/js/layout.js
+++ src/js/layout.js
@@ -28,6 +28,7 @@ const Layout = () => {
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
+						<Route path="/detail" element={<details />} />
 					</Routes>
 					<Footer />
 				</ScrollToTop>
---
+++ src/js/views/details.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useContext } from "react";
+i
+
+export const Details = () => {
+	
+	return (
+		<div>
+			Estamos en el detail
+		</div>
+	);
+};
